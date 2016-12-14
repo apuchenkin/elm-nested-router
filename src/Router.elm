4d1d6bf26aa628a0be9c301f7c6f1f6bf1f01f84
Niklas Hofer <niklas+dev@lanpartei.de>
2016-12-15 00:29:23 +0100

    elm-format (part of elm-upgrade)

--- src/Router.elm
+++ src/Router.elm
@@ -1,4 +1,5 @@
-module Router exposing ( dispatch, initialState )
+module Router exposing (dispatch, initialState)
+
 {-| A simple nested router for single page applications.
 
 See [Example](https://github.com/apuchenkin/elm-nested-router/tree/master/example) ([Live demo](http://apuchenkin.github.io/elm-nested-router/example))
@@ -8,61 +9,89 @@ and [Tests](https://github.com/apuchenkin/elm-nested-router/tree/master/test/Tes
 -}
 
 import Dict
-import Navigation       exposing (Location)
+import Navigation exposing (Location)
+import Router.Matcher as Matcher exposing (Matcher)
+import Router.Types exposing (..)
+import Router.Functions exposing (..)
+import Router.Navigation exposing (..)
 
-import Router.Matcher      as Matcher exposing (Matcher)
-import Router.Types        exposing (..)
-import Router.Functions    exposing (..)
-import Router.Navigation   exposing (..)
 
-{-| Initial state for router. Fed this into your application state -}
+{-| Initial state for router. Fed this into your application state
+-}
 initialState : RouterState route
-initialState = {
-    route = Nothing
-  , params = Dict.empty
-  }
+initialState =
+    { route = Nothing
+    , params = Dict.empty
+    }
 
-{-| Router constructor -}
+
+{-| Router constructor
+-}
 constructor : RouterConfig route state -> Matcher route state -> Router route state
 constructor config matcher =
-  let
-    (RouterConfig c) = config
-    config' = RouterConfig <| { c | routeConfig = matcher.getConfig}
-  in {
-    config = config'
-  , bindForward = bindForward config' matcher
-  , buildUrl = buildUrl config' matcher
-  , forward = forward config' matcher
-  , redirect = redirect config' matcher
-  , match = matchRoute matcher
-  }
+    let
+        (RouterConfig c) =
+            config
+
+        config_ =
+            RouterConfig <| { c | routeConfig = matcher.getConfig }
+    in
+        { config = config_
+        , bindForward = bindForward config_ matcher
+        , buildUrl = buildUrl config_ matcher
+        , forward = forward config_ matcher
+        , redirect = redirect config_ matcher
+        , match = matchRoute matcher
+        }
+
 
 {-| Launches the router.
   Provide `init` function and router config as parameters
- -}
-dispatch : (flags -> (WithRouter route state, Cmd (Action (WithRouter route state)))) -> RouterConfig route (WithRouter route state) -> Program flags -- flags
+-}
+dispatch : (flags -> ( WithRouter route state, Cmd (Action (WithRouter route state)) )) -> RouterConfig route (WithRouter route state) -> Program flags
+
+
+
+-- flags
+
+
 dispatch init config =
-  let
-    (RouterConfig c) = config
-    matcher = Matcher.matcher config
-    router = constructor config matcher
+    let
+        (RouterConfig c) =
+            config
 
-    getHandlers = createHandlers router matcher
-    render' = render router <| List.map getHandlers << matcher.traverse
-    urlUpdate route = runAction <| transition router matcher getHandlers route
+        matcher =
+            Matcher.matcher config
 
-    parser = Navigation.makeParser <| matcher.match << getPath config
-    init' flags route =
-      let
-        (state, cmd) = init flags
-        (state', cmd') = urlUpdate route state
-      in (state', Cmd.batch [cmd, cmd'])
-  in
-    Navigation.programWithFlags parser
-    {
-      init = init'
-    , update = runAction
-    , urlUpdate = urlUpdate
-    , view = render'
-    , subscriptions = c.subscriptions
-    }
+        router =
+            constructor config matcher
+
+        getHandlers =
+            createHandlers router matcher
+
+        render_ =
+            render router <| List.map getHandlers << matcher.traverse
+
+        urlUpdate route =
+            runAction <| transition router matcher getHandlers route
+
+        parser =
+            Navigation.makeParser <| matcher.match << getPath config
+
+        init_ flags route =
+            let
+                ( state, cmd ) =
+                    init flags
+
+                ( state_, cmd_ ) =
+                    urlUpdate route state
+            in
+                ( state_, Cmd.batch [ cmd, cmd_ ] )
+    in
+        Navigation.programWithFlags parser
+            { init = init_
+            , update = runAction
+            , urlUpdate = urlUpdate
+            , view = render_
+            , subscriptions = c.subscriptions
+            }
